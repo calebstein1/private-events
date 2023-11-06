6447a275262724c9501061033bff139d31aa7f49
Caleb Stein <caleb@calebstein.net>
2023-11-06 14:17:27 -0800

    added scoping for past and future events on the model

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,15 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User", foreign_key: "user_id"
   has_many :attended_events
   has_many :attendees, through: :attended_events
+
+  scope :past, -> { where(date: ..(Time.now.midnight)) }
+  scope :future, -> { where(date: (Time.now.midnight - 1.day)..) }
+
+  def past?
+    Date.parse(date).past?
+  end
+
+  def future?
+    Date.parse(date).future?
+  end
 end
